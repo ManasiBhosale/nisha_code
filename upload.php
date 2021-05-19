114fa605728b9b81a742c6d92f96ceefa64019f7
ManasiBhosale <69459173+ManasiBhosale@users.noreply.github.com>
2021-05-19 18:07:41 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,10 +1,10 @@
 <?php
 define('UPLOAD_DIR', 'uploads/');
-$img = $_POST['base64image'];
-$img = str_replace('data:image/jpeg;base64,', '', $img);
+$img = $_POST['camera--output'];
+$img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = UPLOAD_DIR . uniqid() . '.png';
+$file = UPLOAD_DIR . uniqid() . '.jpg';
 $success = file_put_contents($file, $data);
 print $success ? $file : 'Unable to save the file.';
 ?>
